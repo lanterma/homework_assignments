4de7cf88044f2d9dbdadf4f6e0eaa0ed4de9dbe7
Matt Lanterman <Lanterma@gmail.com>
2015-02-16 20:37:39 -0700

    Added changed to number_of_seconds

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -11,8 +11,10 @@ seconds_in_a_day = 60 * 60 * 24
 seconds_in_a_week = seconds_in_a_day * 7
 seconds_in_a_year = seconds_in_a_week *52
 
-# seconds_in_a_day = seconds_in_a_minute * minutes_in_a_hour * hours_in_a_day
-# seconds_in_a_year = seconds_in_a_day * 365
+puts "There are #{seconds_in_a_minute} in a minute"
+puts "There are #{minutes_in_a_hour} in a hour"
+puts "There are #{hours_in_a_day} in a day"
+puts "There are #{days_in_a_week}in a week"
 
 puts  "That means there are:"
 puts  "  #{seconds_in_a_minute * minutes_in_a_hour} seconds in an hour,"
@@ -23,6 +25,11 @@ That means when you turn 20, you've been alive for #{20 * seconds_in_a_year} sec
 and if you make it to 100, you will have lived for #{100 * seconds_in_a_year} seconds. Make them count!
 
 	    """
+
+# There are 60 seconds in a minute
+# There are 60 minutes in an hour
+# There are 24 hours in a day
+# There are 7 days in a week
 
 # 3600 seconds in an hour,
 #	86400 seconds in an day,
