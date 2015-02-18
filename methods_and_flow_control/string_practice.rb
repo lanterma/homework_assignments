86f944f31f01c110a25d21a10cf5bb8289b10198
Matt Lanterman <Lanterma@gmail.com>
2015-02-17 22:29:50 -0700

    Added string_practice

---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,31 @@
+#1/usr/bin/env ruby
+
+practice_string = "this is a string to practice with"
+
+puts "#{practice_string}"
+puts "#{practice_string}".capitalize
+puts "#{practice_string}".upcase
+puts "#{practice_string}"
+puts "The string '#{practice_string}' has #{practice_string.length} characters"
+puts "#{practice_string.reverse}"
+	if practice_string.include?'practice'
+	puts "practice practice practice!"
+	end
+
+
+
+
+
+
+
+
+
+
+
+# this is a string to practice with
+# This is a string to practice with
+# THIS IS A STRING TO PRACTICE WITH
+# This is a 'string' to practice with
+# The string 'this is a string to practice with' has 33 characters
+# htiw ecitcarp ot gnirts a si siht
+# practice practice practice!
