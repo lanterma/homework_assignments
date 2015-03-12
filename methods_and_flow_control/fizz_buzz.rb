ec1ddba64e002a5e61b768047d5f06f498873fb0
Matt Lanterman <Lanterma@gmail.com>
2015-03-11 18:10:51 -0600

    Added && instead of and
    [#88656112]

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 (1..100).each do |number|
-  if number % 5 == 0 and number % 3 == 0
+  if number % 5 == 0 && number % 3 == 0
     puts "FizzBuzz"
   elsif number % 5 == 0
     puts "Buzz"
